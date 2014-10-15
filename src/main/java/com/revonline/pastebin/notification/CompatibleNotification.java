fd22e875658207ea9f71d73d126512779ed936d7
Marco Acierno <marcoaciernoemail@gmail.com>
2014-10-15 21:06:34 +0200

    Nuova classe per gestire le Notifiche

---
+++ src/main/java/com/revonline/pastebin/notification/CompatibleNotification.java
@@ -0,0 +1,29 @@
+package com.revonline.pastebin.notification;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import com.revonline.pastebin.ShareCodeActivity;
+
+/**
+ * Unica classe per gestire le notifiche su tutti
+ * i devices con tutti i livelli di API.
+ *
+ * @see CompatibleNotification#createNotification(android.content.Context) per creare una notifica senza la necessità
+ * di eseguire nessun controllo sul livello API attuale
+ */
+public abstract class CompatibleNotification {
+    public static CompatibleNotification createNotification(Context context) {
+        return ShareCodeActivity.apiLower11 ? new RecentNotificationAPI(context) :
+                new OlderNotificationAPI(context);
+    }
+
+    public abstract CompatibleNotification setContentText(String text);
+    public abstract CompatibleNotification setSmallIcon(int icon);
+    public abstract CompatibleNotification setSmallIcon(int icon, int level);
+    public abstract CompatibleNotification setContentTitle(String title);
+    public abstract CompatibleNotification setContentIntent(PendingIntent pendingIntent);
+    public abstract CompatibleNotification setAutoCancel(boolean autoCancel);
+    public abstract Notification create();
+}
---
+++ src/main/java/com/revonline/pastebin/notification/RecentNotificationAPI.java
@@ -0,0 +1,67 @@
+package com.revonline.pastebin.notification;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+/**
+ * Un'implementazione della classe CompatibleNotification
+ * che fa da wrapper alla API di Android.
+ *
+ * @see com.revonline.pastebin.notification.CompatibleNotification Per maggior informazioni su come utilizzare questa
+ * classe
+ */
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class RecentNotificationAPI extends CompatibleNotification {
+    private final Context context;
+    private Notification.Builder builder;
+
+    RecentNotificationAPI(final Context context) {
+        this.context = context;
+        builder = new Notification.Builder(context);
+    }
+
+    @Override
+    public CompatibleNotification setContentText(String text) {
+        builder.setContentText(text);
+        return this;
+    }
+
+    @Override
+    public CompatibleNotification setSmallIcon(int icon) {
+        builder.setSmallIcon(icon);
+        return this;
+    }
+
+    @Override
+    public CompatibleNotification setSmallIcon(int icon, int level) {
+        builder.setSmallIcon(icon, level);
+        return this;
+    }
+
+    @Override
+    public CompatibleNotification setContentTitle(String title) {
+        builder.setContentTitle(title);
+        return this;
+    }
+
+    @Override
+    public CompatibleNotification setContentIntent(PendingIntent pendingIntent) {
+        builder.setContentIntent(pendingIntent);
+        return this;
+    }
+
+    @Override
+    public CompatibleNotification setAutoCancel(boolean autoCancel) {
+        builder.setAutoCancel(autoCancel);
+        return this;
+    }
+
+    @Override
+    public Notification create() {
+        return builder.build();
+    }
+}
