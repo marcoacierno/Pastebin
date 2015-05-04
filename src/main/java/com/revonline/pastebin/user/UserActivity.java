c12696b820fe9ade2954075a5476b21083486d97
Marco Acierno <marcoaciernoemail@gmail.com>
2015-05-04 19:42:11 +0200

    Add "Import settings from Pastebin" during login to allow user to user the same settings
    Fix #46

--- src/main/java/com/revonline/pastebin/user/UserActivity.java
+++ src/main/java/com/revonline/pastebin/user/UserActivity.java
@@ -6,9 +6,11 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,6 +19,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -41,6 +44,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
@@ -79,6 +83,7 @@ public class UserActivity extends Activity {
   private boolean showLocalPastes;
   private DownloadUserPastes downloadUserPastesTask;
   private MenuItem showLocalPastesMenuItem;
+  private CheckBox importSettings;
 
   @Override
   public void onCreate(Bundle savedInstanceHere) {
@@ -102,6 +107,8 @@ public class UserActivity extends Activity {
       username = (EditText) findViewById(R.id.username);
       password = (EditText) findViewById(R.id.password);
       loginButton = (Button) findViewById(R.id.loginbutton);
+
+      importSettings = (CheckBox) findViewById(R.id.importSettings);
     } else {
       setContentView(R.layout.iopastes);
       // ID: mypastes
@@ -571,31 +578,92 @@ public class UserActivity extends Activity {
 
       String userKey = null;
 
-      HttpClient client = new DefaultHttpClient();
-      HttpPost post = new HttpPost("http://pastebin.com/api/api_login.php");
-      HttpResponse response;
+      {
 
-      ArrayList<BasicNameValuePair> pair = new ArrayList<>();
-      pair.add(new BasicNameValuePair("api_dev_key", SpecialKeys.DEV_KEY));
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost("http://pastebin.com/api/api_login.php");
+        HttpResponse response;
 
-      try {
-        pair.add(new BasicNameValuePair("api_user_name", URLEncoder.encode(name, "ISO-8859-1")));
-        pair.add(new BasicNameValuePair("api_user_password", URLEncoder.encode(password, "ISO-8859-1")));
+        ArrayList<BasicNameValuePair> pair = new ArrayList<>();
+        pair.add(new BasicNameValuePair("api_dev_key", SpecialKeys.DEV_KEY));
 
-        post.setEntity(new UrlEncodedFormEntity(pair));
+        try {
+          pair.add(new BasicNameValuePair("api_user_name", URLEncoder.encode(name, "ISO-8859-1")));
+          pair.add(new BasicNameValuePair("api_user_password", URLEncoder.encode(password, "ISO-8859-1")));
 
-        response = client.execute(post);
-        StatusLine line = response.getStatusLine();
+          post.setEntity(new UrlEncodedFormEntity(pair));
 
-        if (line.getStatusCode() == HttpStatus.SC_OK) {
-          // OK
-          ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-          response.getEntity().writeTo(outputStream);
-          outputStream.close();
-          userKey = outputStream.toString();
+          response = client.execute(post);
+          StatusLine line = response.getStatusLine();
+
+          if (line.getStatusCode() == HttpStatus.SC_OK) {
+            // OK
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            response.getEntity().writeTo(outputStream);
+            outputStream.close();
+            userKey = outputStream.toString();
+          }
+        } catch (IOException e) {
+          Log.e("UserActivity", "exception while login", e);
         }
-      } catch (IOException e) {
-        Log.e("UserActivity", "exception while login", e);
+
+      }
+
+      Log.d("UserActivity", "check if I should import settings");
+      Log.d("UserActivity", "import settings is selected? " + importSettings.isChecked());
+      Log.d("UserActivity", "userKey is null? " + (userKey == null));
+      Log.d("UserActivity", "userKey is? " + userKey);
+      if (importSettings.isChecked() && userKey != null && !ErrorMessages.containsError(userKey)) {
+        Log.d("UserActivity", "user asked us to import his settings and we can");
+
+        // http://pastebin.com/api/api_post.php
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost("http://pastebin.com/api/api_post.php");
+        HttpResponse response;
+
+        ArrayList<BasicNameValuePair> pair = new ArrayList<>();
+        pair.add(new BasicNameValuePair("api_dev_key", SpecialKeys.DEV_KEY));
+
+        try {
+          pair.add(new BasicNameValuePair("api_option", "userdetails"));
+          pair.add(new BasicNameValuePair("api_user_key", userKey));
+
+          post.setEntity(new UrlEncodedFormEntity(pair));
+
+          response = client.execute(post);
+          StatusLine line = response.getStatusLine();
+
+          if (line.getStatusCode() == HttpStatus.SC_OK) {
+            // OK
+            final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            response.getEntity().writeTo(outputStream);
+
+            Log.d("UserActivity", "ok received user preferences");
+
+            final DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            final DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            final Document document = documentBuilder.parse(new ByteArrayInputStream(outputStream.toByteArray()));
+
+            final Element root = document.getDocumentElement();
+
+            final SharedPreferences.Editor preferences = PreferenceManager.getDefaultSharedPreferences(UserActivity.this).edit();
+
+            preferences.putString("pref_defaultexpiration", root.getElementsByTagName("user_expiration").item(0).getTextContent());
+            preferences.putString("pref_defaultlanguage", root.getElementsByTagName("user_format_short").item(0).getTextContent());
+
+            Log.d("UserActivity", "default expiration => " + root.getElementsByTagName("user_expiration").item(0).getTextContent());
+            Log.d("UserActivity", "pref_defaultlanguage => " + root.getElementsByTagName("user_format_short").item(0).getTextContent());
+
+
+            preferences.commit();
+
+            outputStream.close();
+          }
+        } catch (IOException e) {
+          Log.e("UserActivity", "exception while getting user settings", e);
+        } catch (ParserConfigurationException | SAXException e) {
+          Log.e("UserActivity", "exception while getting user settings (creating xml parser)", e);
+        }
       }
 
       return userKey;
